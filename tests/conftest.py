15e3113b12da6b6fd40a68645ead860098fe20c8
Carlos <CarlosMaciasCusiter@gmail.com>
2022-07-03 13:52:09 -0700

    replaced @ with : in database url

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = f"postgres://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
+SQLALCHEMY_DATABASE_URL = f"postgres://{settings.database_username}:{settings.database_password}:{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
