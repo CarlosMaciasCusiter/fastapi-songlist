06b0b64e6ab9c4ac83c91826f007740539a463c1
Carlos <CarlosMaciasCusiter@gmail.com>
2022-07-03 12:36:11 -0700

    parsing port as an int

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import sessionmaker
 from app.oauth2 import create_accesss_token
 from app import models
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{int(settings.database_port)}/{settings.database_name}_test"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
